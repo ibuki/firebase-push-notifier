83b1ba1c157931443ccd2511e7f655fc77463ac8
Ibuki Suzuki <ibuki.nakamura@gmail.com>
2023-04-13 18:11:49 +0900

    Fix firebase app initializing

--- functions/src/utils.ts
+++ functions/src/utils.ts
@@ -4,7 +4,7 @@ import * as admin from "firebase-admin";
  * @param {string[]} tokens
  */
 export async function sendMulticast(tokens: string[]) {
-  admin.initializeApp();
+  admin.apps.length || admin.initializeApp();
   const message = {
     notification: {title: "sendMessageSample", body: new Date().toISOString()},
     tokens,
