1b7fa764c86e89191fc0db4c909699e8c0c7a3d9
Ibuki Suzuki <ibuki.nakamura@gmail.com>
2023-04-26 16:23:07 +0900

    Use VITE env

--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -6,6 +6,7 @@ import { firebaseConfig } from '../config';
 import { useAuthState as useAuthStateBase } from 'react-firebase-hooks/auth';
 
 const app = initializeApp(firebaseConfig);
+// FIXME: It becomes blank screen when wrong firebase config.
 const auth = getAuth(app)
 export const signIn = (email: string, password: string) => signInWithEmailAndPassword(auth, email, password)
 export const signOut = () => firebaseSignOut(auth)
