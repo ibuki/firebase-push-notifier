1aef5094c0e083e4a64b6487efbcfbba5310cb57
Ibuki Suzuki <ibuki.nakamura@gmail.com>
2023-04-27 10:39:26 +0900

    sendPushNotification

--- functions/src/controllers/pushNotificationController.ts
+++ functions/src/controllers/pushNotificationController.ts
@@ -1,5 +1,14 @@
+import * as admin from "firebase-admin";
 import {RequestHandler} from "express";
+import {sendMulticast} from "../utils";
 
 export const publishPushNotification: RequestHandler = async (req, res) => {
+  const pushNotificationId = req.params.pushNotificationId;
+  const doc = await admin.firestore().collection("pushNotifications").doc(pushNotificationId).get();
+  const deviceTokenGroupId = doc.data()?.deviceTokenGroupId;
+  const snapshot = await admin.firestore().collection("deviceTokenGroups").doc(deviceTokenGroupId).collection("deviceTokens").limit(1000).get();
+  const deviceTokens = snapshot.docs.map((doc) => doc.data());
+  const targets = deviceTokens.map((deviceToken) => deviceToken.token);
+  await sendMulticast(targets);
   return res.json({message: req.params.pushNotificationId});
 };
