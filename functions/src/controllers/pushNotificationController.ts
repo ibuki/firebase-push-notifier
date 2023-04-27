08205516ece639e5f2a7fbfe2440cc299dc71c2b
Ibuki Suzuki <ibuki.nakamura@gmail.com>
2023-04-27 10:59:46 +0900

    Put content

--- functions/src/controllers/pushNotificationController.ts
+++ functions/src/controllers/pushNotificationController.ts
@@ -5,10 +5,10 @@ import {sendMulticast} from "../utils";
 export const publishPushNotification: RequestHandler = async (req, res) => {
   const pushNotificationId = req.params.pushNotificationId;
   const doc = await admin.firestore().collection("pushNotifications").doc(pushNotificationId).get();
-  const deviceTokenGroupId = doc.data()?.deviceTokenGroupId;
-  const snapshot = await admin.firestore().collection("deviceTokenGroups").doc(deviceTokenGroupId).collection("deviceTokens").limit(1000).get();
+  const pushNotification = doc.data();
+  const snapshot = await admin.firestore().collection("deviceTokenGroups").doc(pushNotification?.deviceTokenGroupId).collection("deviceTokens").limit(1000).get();
   const deviceTokens = snapshot.docs.map((doc) => doc.data());
   const targets = deviceTokens.map((deviceToken) => deviceToken.token);
-  await sendMulticast(targets);
+  await sendMulticast(targets, pushNotification?.content);
   return res.json({message: req.params.pushNotificationId});
 };
--- functions/src/utils.ts
+++ functions/src/utils.ts
@@ -2,11 +2,12 @@ import * as admin from "firebase-admin";
 /**
  * sendMulticast
  * @param {string[]} tokens
+ * @param {string} body
  */
-export async function sendMulticast(tokens: string[]) {
+export async function sendMulticast(tokens: string[], body = "") {
   admin.apps.length || admin.initializeApp();
   const message = {
-    notification: {title: "sendMessageSample", body: new Date().toISOString()},
+    notification: {title: "sendMessageSample", body: body || new Date().toISOString()},
     tokens,
   };
   const response = await admin.messaging().sendMulticast(message);
